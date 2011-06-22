1e8a5f3da8d6f9b7240ec49c0a3b39c9a1db8fb8
Neill Bogie <neillbogie@googlemail.com>
2011-06-22 20:44:41 +0100

    tidied types

--- Types.hs
+++ Types.hs
@@ -4,8 +4,21 @@ import Data.Text (Text(..), pack, unpack)
 import Data.Map as M
 
 type KHash = Text
-data KFile = KFile String KHash deriving (Show)
-data KindleCollections = KindleCollections CollMap deriving (Show)
+
+data KFile = 
+  KFile String KHash 
+  deriving (Show)
+
+data KindleCollections = 
+  KindleCollections CollMap 
+  deriving (Show)
+
 type CollMap = Map Text Collection
-data Collection = Collection [Text] Integer deriving (Show)
-data AnnotCollection = AnnotCollection Text [(KHash, Maybe FilePath)] Integer deriving (Show)
+
+data Collection = 
+  Collection [Text] Integer 
+  deriving (Show)
+
+data AnnotCollection = 
+  AnnotCollection Text [(KHash, Maybe FilePath)] Integer 
+  deriving (Show)
